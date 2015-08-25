f66e670978167d9c1d13c077e89e2fd9e0e518f6
raviqqe <raviqqe@gmail.com>
2015-08-25 02:57:56 +0000

    Parallelize grep

--- with.sh
+++ with.sh
@@ -6,17 +6,20 @@ then
   exit 1
 fi
 
+
+# main routine
+
 for expr in "$@"
 do
   exprs=$exprs" -e \"$expr\""
 done
 
-for file in $(find)
+for file in $(find .)
 do
-  if [ -z "$(echo "$file" | grep -e "/\." -e "$\.[^/]")" ]
+  if [ -z "$(echo "$file" | grep -e "/\." -e "$\.[^/]")" ] || [ -r "$file" ]
   then
-    files=$files" \"$file\""
+    eval grep -nHI "$exprs" \""$file"\" &
   fi
 done
 
-test -r "$file" && eval grep -nHIT $exprs $files
+wait
